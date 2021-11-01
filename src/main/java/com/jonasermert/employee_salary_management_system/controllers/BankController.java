9ce10732dccfe08cff5f93110a158dc27a659901
Jonas Ermert <ermert.jonas@yahoo.de>
2021-11-01 10:02:15 +0100

    UserDTO implemented Files created

---
+++ src/main/java/com/jonasermert/employee_salary_management_system/controllers/BankController.java
@@ -0,0 +1,4 @@
+package com.jonasermert.employee_salary_management_system.controllers;
+
+public class BankController {
+}
---
+++ src/main/java/com/jonasermert/employee_salary_management_system/dtos/UserDto.java
@@ -0,0 +1,99 @@
+package com.jonasermert.employee_salary_management_system.dtos;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class UserDto implements Serializable {
+    private String username;
+    private String password;
+    private List<String> authorityNames;
+    private String fullName;
+    private String email;
+    private Integer grade;
+    private String address;
+    private String phoneNumber;
+    private Boolean active = true;
+    private String profilepicture = "";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getAuthorityNames() {
+        return authorityNames;
+    }
+
+    public void setAuthorityNames(List<String> authorityNames) {
+        this.authorityNames = authorityNames;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getPropic() {
+        return propic;
+    }
+
+    public void setPropic(String propic) {
+        this.propic = propic;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
